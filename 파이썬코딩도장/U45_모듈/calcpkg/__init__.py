a8e0663497e94cd03dca051691fa6c65ce8315af
Yeongil Kim <skyksdc01@gmail.com>
2020-11-05 15:51:29 +0900

    모듈

---
+++ 파이썬코딩도장/U45_모듈/calcpkg/__init__.py
@@ -0,0 +1,9 @@
+# 최초는 빈 파일
+
+# 두 번째
+#from . import operation
+#from . import geometry
+
+# 세 번째
+from .operation import *
+from .geometry import *
---
+++ 파이썬코딩도장/U45_모듈/main2.py
@@ -0,0 +1,7 @@
+from calcpkg import *
+
+print(add(10, 20))    # operation 모듈의 add 함수 사용
+print(mul(10, 20))    # operation 모듈의 mul 함수 사용
+ 
+print(triangle_area(30, 40))    # geometry 모듈의 triangle_area 함수 사용
+print(rectangle_area(30, 40))   # geometry 모듈의 rectangle_area 함수 사용
